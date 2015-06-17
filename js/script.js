3ade8c3f8c566b8e0877553cc5abb60fd0e1a4c5
Armando Shala <armandoshala@Armando-Shalas-Retina.local>
2015-06-17 11:17:45 +0200

    validate date

--- js/script.js
+++ js/script.js
@@ -17,6 +17,16 @@ $(document).ready(function () {
     google.maps.event.addDomListener(window, 'load', initialize);
     fillDetailPageWithData();
     $("#save").click(addNewEintrag);
+    $(function () {
+        $('#save').bind('click', function () {
+            var txtVal = $('#txtDate').val();
+            if (isDate(txtVal)) {
+                alert('Valid Date');
+            } else {
+                alert('Invalid Date');
+            }
+        });
+    });
 
 });
 
@@ -167,7 +177,7 @@ function addNewEintrag() {
     var inLat = $("#name").val();
     var inLng = "";
     var inGeschaeft = "";
-    var inDate = "";
+    var inDate = $("#");
     var inIsWichtig = "";
     var inIsDone = "";
 
@@ -186,4 +196,36 @@ function geolocate() {
             autocomplete.setBounds(circle.getBounds());
         });
     }
+}
+
+
+function isDate(txtDate) {
+
+    var currVal = txtDate;
+
+    if (currVal == '') {
+        return false;
+    }
+    //Declare Regex 
+    var rxDatePattern = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/;
+    var dtArray = currVal.match(rxDatePattern); // is format OK?
+    if (dtArray === null) {
+        return false;
+    }
+    //Checks for mm/dd/yyyy format.
+    dtMonth = dtArray[1];
+    dtDay = dtArray[3];
+    dtYear = dtArray[5];
+    if (dtMonth < 1 || dtMonth > 12) {
+        return false;
+    } else if (dtDay < 1 || dtDay > 31) {
+        return false;
+    } else if ((dtMonth == 4 || dtMonth == 6 || dtMonth == 9 || dtMonth == 11) && dtDay == 31) {
+        return false;
+    } else if (dtMonth == 2) {
+        var isleap = (dtYear % 4 == 0 && (dtYear % 100 != 0 || dtYear % 400 == 0));
+        if (dtDay > 29 || (dtDay == 29 && !isleap))
+            return false;
+    }
+    return true;
 }
